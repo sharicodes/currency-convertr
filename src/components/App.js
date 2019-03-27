2a3ea5cc85c6d9324d277d3acdc1f9142b51b05d
sharicodes <srosenb@yahoo.com>
2019-03-27 13:11:37 -0400

    notsure

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,78 @@ import React, { Component } from "react";
 import "../css/App.css";
 import NavBar from "./NavBar";
 import CurrentDataContainer from "./CurrentDataContainer";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <NavBar />
-        <CurrentDataContainer />
+function App() {
+  return (
+    <Router>
+      <div>
+        <Home />
+
+        <Route exact path="/" component={NavBar} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
       </div>
-    );
-  }
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Topic({ match }) {
+  return <h3>Requested Param: {match.params.id}</h3>;
+}
+
+function Topics({ match }) {
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.path}/:id`} component={Topic} />
+      <Route
+        exact
+        path={match.path}
+        render={() => <h3>Please select a topic.</h3>}
+      />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <React.Fragment>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+      </ul>
+    </React.Fragment>
+  );
 }
 
 export default App;
+// <div className="App">
+//   <NavBar />
+//   <CurrentDataContainer />
+// </div>
